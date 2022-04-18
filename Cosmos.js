c2a5dcb2fe3cb230c23ed622b0957f01ea2c43d2
Glist2 <103953500+Glist2@users.noreply.github.com>
2022-04-18 22:15:02 +0500

    Create Cosmos.js
    test

---
+++ Cosmos.js
@@ -0,0 +1,8 @@
+@@ -1,6 +1,6 @@
+{
+  "name": "Cosmos.js",
+  "version": "2.0.3",
+  "version": "2.0.4",
+  "description": "An HTML5 saveAs() FileSaver implementation",
+  "main": "dist/FileSaver.min.js",
+  "files": [
